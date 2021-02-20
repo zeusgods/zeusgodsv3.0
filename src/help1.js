2129ea19525c88da9f63961e03cf1472ed84acd5
zeusgods <79343597+zeusgods@users.noreply.github.com>
2021-02-20 14:17:02 -0200

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,49 @@
+const help1 = (prefix) => {
+
+	return `
+╭──────────────╮
+*  𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐃𝐎 𝐙𝐄𝐔𝐒    *
+╰──────────────╯
+ 
+╠➣*${prefix}marcar*
+╠➣ *${prefix}marcar2*
+╠➣ *${prefix}marcar3*
+╠➣ *${prefix}loli*
+╠➣ *${prefix}loli1*
+╠➣ *${prefix}hentai*
+╠➣ *${prefix}dono*
+╠➣ *${prefix}porno*
+╠➣ *${prefix}boanoite*
+╠➣ *${prefix}bomdia*
+╠➣ *${prefix}boatarde*
+╠➣ *${prefix}mia*
+╠➣ *${prefix}mia1*
+╠➣ *${prefix}mia2*
+╠➣ *${prefix}belle*
+╠➣ *${prefix}belle1*
+╠➣ *${prefix}belle2*
+╠➣ *${prefix}belle3*
+╠➣*${prefix}akeno*
+╠➣ *${prefix}meme*
+╠➣ *${prefix}lofi*
+╠➣ *${prefix}malkova*
+╠➣ *${prefix}canal*
+╠➣ *${prefix}nsfwloli*
+╠➣ *${prefix}reislin*
+╠➣ *${prefix}limpar*
+╠➣ *${prefix}marcar*
+╠➣ *${prefix}ts (texto que deseja transmitir)*
+
+═════✪〘 𝐙𝐄𝐔𝐒 〙✪═════
+
+*𝐙𝐄𝐔𝐒* 🤗
+*Digite ${prefix}dono para mais info*
+
+═════✪〘 𝐙𝐄𝐔𝐒 〙✪═════
+`
+
+}
+exports.help1 = help1
+
+
+
---
+++ src/zeusmenu.js
@@ -0,0 +1,43 @@
+const zeusmenu = (prefix) => {
+	return `
+
+          𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝙳𝙾 𝙱𝙾𝚃 😡
+
+
+  *𝙰𝙸 𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  𝙴𝚍𝚒𝚝𝚘𝚛: 𝚉𝚎𝚞𝚜
+  𝙳𝚞𝚟𝚒𝚍𝚊𝚜? 𝚂𝚎 𝚏𝚞𝚍𝚎𝚞 👇
+  WA.me/5527996463517
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
+
+
+
